d987ff021ca60fd9747e81fc8afbe026dafd18cb
eriklp <lopez.erik@correounivalle.edu.co>
2024-09-19 13:06:51 -0500

    feature: add and fixes tf scripts

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,9 +7,3 @@ output "db_name" {
   description = "The name of the MySQL database"
   value       = aws_db_instance.franquicias_db.db_name
 }
-
-output "db_password" {
-  description = "Password of database"
-  value = aws_db_instance.franquicias_db.password
-  sensitive = true
-}
